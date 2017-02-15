0597fba48ab94673f1b1ff7949c06a2edb2b0b76
Jeff Carbonella <jeff@contactually.com>
2017-02-15 10:44:30 -0500

    Add android adbkey to lastpass sync

--- lastpass-pull.sh
+++ lastpass-pull.sh
@@ -3,3 +3,7 @@
 FILENAME="symlinks/.bashrc.secrets"
 LPASS_NAME="Secure Notes/dotfiles/.bashrc.secrets"
 lpass show "${LPASS_NAME}" &> /dev/null && lpass show --notes "${LPASS_NAME}" > $FILENAME
+
+FILENAME="symlinks/.android/adbkey"
+LPASS_NAME="Secure Notes/dotfiles/.android-adbkey"
+lpass show "${LPASS_NAME}" &> /dev/null && lpass show --notes "${LPASS_NAME}" > $FILENAME
--- lastpass-push.sh
+++ lastpass-push.sh
@@ -2,4 +2,10 @@
 
 FILENAME="symlinks/.bashrc.secrets"
 LPASS_NAME="Secure Notes/dotfiles/.bashrc.secrets"
-cat $FILENAME | lpass edit --sync=now --non-interactive --notes "${LPASS_NAME}"
+cat $FILENAME | lpass edit --non-interactive --notes "${LPASS_NAME}"
+
+FILENAME="symlinks/.android/adbkey"
+LPASS_NAME="Secure Notes/dotfiles/.android-adbkey"
+cat $FILENAME | lpass edit --non-interactive --notes "${LPASS_NAME}"
+
+lpass sync
